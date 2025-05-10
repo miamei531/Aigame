2468b8c8d678ce40f0a88f80af582815e58761c8
hvie123 <viet11222008@Gmail.com>
2025-05-11 02:16:06 +0700

    toi lam menu nha

--- học cách làm j đó/cảnh_hả.gd
+++ học cách làm j đó/cảnh_hả.gd
@@ -6,12 +6,11 @@ extends Node2D
 
 
 func _on_pờ_lay_pressed():
-	get_tree().change_scene_to_file("res://man_1.tscn")
+	get_tree().change_scene_to_file("res://menu.tscn")
 	
 func _ready():
 	#Utils.savegame()
 	Utils.loadgame()
 
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://man_2/man_2.tscn")
+
