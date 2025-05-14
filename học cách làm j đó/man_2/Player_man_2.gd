9b09c1125f21364b3b4b443143a44317b978d2f7
hvie123 <viet11222008@Gmail.com>
2025-05-14 17:06:53 +0700

    toi bi chích điện

--- học cách làm j đó/man_2/Player_man_2.gd
+++ học cách làm j đó/man_2/Player_man_2.gd
@@ -1,27 +1,38 @@
 extends CharacterBody2D
 
+var positions = [Vector2(536, 320), Vector2(536, 480)]
+var positions_ngang=[Vector2(400,480),Vector2(536,480),Vector2(600,480)]
+var current_index = 0
+var check = 0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready():
+	position = positions[current_index]
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 0
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_up") and check == 0 :
+		jump_to_next_position()
+	if Input.is_action_just_pressed("ui_left") and position.y == 480:
+		jump_to_left()
+	if Input.is_action_just_pressed("ui_right") and position.y == 480:
+		jump_to_right()
+func jump_to_next_position():
+	# Không kiểm tra tween đang chạy nữa (đơn giản hoá)
+	if current_index < positions.size() - 1:
+		current_index += 1
+		var target_position = positions[current_index]
+		var tween = create_tween()
+		tween.tween_property(self, "position", target_position, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+func jump_to_left():
+	if check!= -1 :
+		check-=1
+		var index=1+check
+		var target_position = positions_ngang[index]
+		var tween = create_tween()
+		tween.tween_property(self, "position", target_position, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+func jump_to_right():
+	if check!= 1 :
+		check+=1
+		var index=1+check
+		var target_position = positions_ngang[index]
+		var tween = create_tween()
+		tween.tween_property(self, "position", target_position, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
