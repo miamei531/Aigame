27c0dc19392114e25ca75d1d476f5d4291d96695
hvie123 <viet11222008@Gmail.com>
2025-05-14 21:57:20 +0700

    update

--- học cách làm j đó/man_2/Player_man_2.gd
+++ học cách làm j đó/man_2/Player_man_2.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 var positions = [Vector2(536, 320), Vector2(536, 480)]
-var positions_ngang=[Vector2(400,480),Vector2(536,480),Vector2(600,480)]
+var positions_ngang=[Vector2(216,480),Vector2(536,480),Vector2(856,480)]
 var current_index = 0
 var check = 0
-
+var is_facing_front = true
 func _ready():
 	position = positions[current_index]
+	$AnimatedSprite2D.play("front")
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_up") and check == 0 :
@@ -15,10 +16,20 @@ func _process(_delta):
 		jump_to_left()
 	if Input.is_action_just_pressed("ui_right") and position.y == 480:
 		jump_to_right()
+	if Input.is_action_just_pressed("ui_down") and position.y == 480:
+		$AnimatedSprite2D.play("back")   # quay lưng lại
+		is_facing_front = false
+
+
 func jump_to_next_position():
 	# Không kiểm tra tween đang chạy nữa (đơn giản hoá)
-	if current_index < positions.size() - 1:
+	if current_index < positions.size() - 1 and is_facing_front:
 		current_index += 1
+		var target_position = positions[current_index]
+		var tween = create_tween()
+		tween.tween_property(self, "position", target_position, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	if current_index > 0 and !is_facing_front:
+		current_index -= 1
 		var target_position = positions[current_index]
 		var tween = create_tween()
 		tween.tween_property(self, "position", target_position, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
--- học cách làm j đó/man_2/man_2.gd
+++ học cách làm j đó/man_2/man_2.gd
@@ -1,11 +1,37 @@
 extends Node2D
 
 
+var mob_scenes = [
+	preload("res://man_2/cow.tscn"),
+	preload("res://man_2/chicken.tscn"),
+]
+
+var current_mob: Node = null  # Mob hiện tại (duy nhất)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	spawn_unique_mob()
+# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func _on_timer_timeout():
+	spawn_unique_mob()
+func spawn_unique_mob():
+	# Xoá mob cũ nếu còn tồn tại
+	if current_mob and is_instance_valid(current_mob):
+		current_mob.queue_free()
+
+	# Random mob mới
+	var mob_scene = mob_scenes[randi() % mob_scenes.size()]
+	var mob = mob_scene.instantiate()
+
+	# Đặt vị trí mob 
+	mob.position = Vector2(800, 280)
+	add_child(mob)
+	current_mob = mob
