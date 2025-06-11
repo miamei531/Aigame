ddaf5f17aacd24427822a4df95417c384393b7ce
miamei531 <144574310+miamei531@users.noreply.github.com>
2025-06-11 20:10:00 +0700

    dohoa

--- học cách làm j đó/latvat/letter.gd
+++ học cách làm j đó/latvat/letter.gd
@@ -4,11 +4,30 @@ extends Area2D
 @export var speed := 100.0
 @export var target_letter_label: Label = null 
 @export var diem_label: Label = null
+#func _ready():
+	#$Label.text = letter
+#
+	#set_meta("letter", letter) 
+#
+	#if target_letter_label == null:
+		#print("Chưa gán target_letter_label!")
+#
+	#if diem_label == null:
+		#diem_label = get_node_or_null("$diem")
+		#if diem_label == null:
+			#print("Không tìm thấy node 'diem'!")
 func _ready():
 	$Label.text = letter
+	set_meta("letter", letter)
 
-	set_meta("letter", letter) 
+	# Gán kích thước font ngẫu nhiên từ 60 đến 70
+	var random_size = randi_range(50, 80)
+	var font = $Label.get_theme_font("font") # lấy font hiện tại từ theme
+	var font_override = font.duplicate() # tạo bản sao để không ảnh hưởng toàn hệ thống
+	$Label.add_theme_font_override("font", font)
+	$Label.add_theme_font_size_override("font_size", random_size)
 
+	# Kiểm tra các node liên quan
 	if target_letter_label == null:
 		print("Chưa gán target_letter_label!")
 
