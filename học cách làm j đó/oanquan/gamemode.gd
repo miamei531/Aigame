961ff47aeee99a9191a20d14ac8d75eb59ff4862
hvie123 <viet11222008@Gmail.com>
2025-09-23 22:17:03 +0700

    a

--- học cách làm j đó/oanquan/gamemode.gd
+++ học cách làm j đó/oanquan/gamemode.gd
@@ -1,17 +1,17 @@
 extends Node2D
 @onready var buttons: Array[Button] = [$Button, $Button2, $Button3]
 @onready var khung= $"Kkkkk-removebg-preview"
-var vitri=[Vector2(560,228),Vector2(560,432),Vector2(580,584)]
-var size = [Vector2(1.312,0.528),Vector2(1.312,0.528),Vector2(0.398,0.144)]
+var vitri=[Vector2(560,132),Vector2(560,336),Vector2(560,514),Vector2(89.5,71.5)]
+var size = [Vector2(1.312,0.528),Vector2(1.312,0.528),Vector2(1.312,0.528),Vector2(0.398,0.144)]
 var idx= 0
 func _unhandled_input(event: InputEvent):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_DOWN:
-			idx = (idx+4)%3
+			idx = (idx+5)%4
 			khung.position=vitri[idx]
 			khung.scale = size[idx]
 		elif event.keycode == KEY_UP:
-			idx = (idx+2)%3
+			idx = (idx+3)%4 
 			khung.position=vitri[idx]
 			khung.scale = size[idx]
 		elif event.keycode == KEY_SPACE:     
